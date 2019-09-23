ee67aca5cd57acddfb1acb944e64164b9c7ef946
Henry Zhu <henryzhu@berkeley.edu>
2019-09-23 14:00:09 -0700

    Don't take grad steps on classifier if unused

--- softlearning/algorithms/multi_vice.py
+++ softlearning/algorithms/multi_vice.py
@@ -6,10 +6,11 @@ from .multi_sac_classifier import MultiSACClassifier
 class MultiVICEGAN(MultiSACClassifier):
     def _epoch_after_hook(self, *args, **kwargs):
         losses_per_classifier = [[] for _ in range(self._num_goals)]
-        for i in range(self._n_classifier_train_steps):
-            classifier_index = i % self._num_goals
-            feed_dict = self._get_classifier_feed_dict(classifier_index)
-            losses_per_classifier[classifier_index].append(
-                self._train_classifier_step(classifier_index, feed_dict))
+        for i in range(self._num_goals):
+            if self._ext_reward_coeffs[i]:
+                for _ in range(self._n_classifier_train_steps):
+                    feed_dict = self._get_classifier_feed_dict(i)
+                    losses_per_classifier[i].append(
+                        self._train_classifier_step(i, feed_dict))
         self._training_losses_per_classifier = [
             np.concatenate(loss, axis=-1) for loss in losses_per_classifier]
